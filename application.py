edb670638a41e962f3c02d5a1403d004127831fa
John Loane <john.loane@dkit.ie>
2022-09-29 10:37:32 +0100

    Flask code to deal with success and failure
    Need to modify this code to make sure that people cannot hack the
    site and register for whatever sport they want

--- application.py
+++ application.py
@@ -11,7 +11,11 @@ def index():
         return render_template("greet.html", name=request.form.get("name", "world"))
 
 
-
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    return render_template("success.html")
 
 if __name__ == '__main__':
     app.run()
