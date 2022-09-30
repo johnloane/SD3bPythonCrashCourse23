d8a99bb920ba532a8b06ab63e7899d9c02c94f66
John Loane <john.loane@dkit.ie>
2022-09-30 14:59:55 +0100

    Dealing with errors
    Customising errors that are displayed based on what the user is
    missing

--- application.py
+++ application.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template, request
 
+SPORTS = ["Basketball", "Lawn Tennis", "Soccer", "Table Tennis", "Badminton"]
+
+REGISTRANTS = {}
+
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", sports=SPORTS)
     elif request.method == "POST":
         return render_template("greet.html", name=request.form.get("name", "world"))
 
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
-        return render_template("failure.html")
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Stop messing with my website")
     return render_template("success.html")
 
 if __name__ == '__main__':
